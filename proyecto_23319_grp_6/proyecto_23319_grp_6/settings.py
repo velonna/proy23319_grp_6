df11d737870b40c4b4a9e06b039549ca0b8260ed
v nina <veronica.nina@softtek.com>
2023-04-14 21:53:53 -0300

    primeras pantallas

--- proyecto_23319_grp_6/proyecto_23319_grp_6/settings.py
+++ proyecto_23319_grp_6/proyecto_23319_grp_6/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'proyecto_23319_grp_6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'static'
+] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
